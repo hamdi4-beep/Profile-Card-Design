dcb10da518d4ec8c465eb062735d5a5cbcf9e3a9
hamdi4-beep <75071205+hamdi4-beep@users.noreply.github.com>
2022-03-17 14:51:45 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const init = () => {
+    const wrapper = document.querySelector('.wrapper')
+    const coverImg = wrapper.querySelector('.img-cover img')
+    const addBtn = wrapper.querySelector('.fa-plus-circle')
+
+    addBtn.addEventListener('click', e => {
+        const input = document.createElement('input')
+        input.type = 'file'
+        input.click()
+
+        input.onchange = function(e) {
+            if (e.target.files[0]) {
+                const reader = new FileReader
+                const file = this.files[0]
+                const type = file.type
+
+                if (type.split('/')[0] == 'image') {
+                    reader.onload = function() {
+                        coverImg.src = reader.result
+                    }
+
+                    reader.readAsDataURL(file)
+                }
+            }
+        }
+
+        input.remove()
+    })
+}
+
+window.onload = init
