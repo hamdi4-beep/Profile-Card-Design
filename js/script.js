6497f1be8e0db3f5029ea168405c5242871c3409
hamdi4-beep <75071205+hamdi4-beep@users.noreply.github.com>
2022-03-18 07:26:13 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,50 @@ const init = () => {
     const coverImg = wrapper.querySelector('.img-cover img')
     const addBtn = wrapper.querySelector('.fa-cog')
     const angleUp = wrapper.querySelector('.fa-angle-up')
+    const dialog = wrapper.querySelector('dialog')
+    const likesContainer = wrapper.querySelector('.likes-container')
+    const followersContainer = wrapper.querySelector('.followers-container')
+    const header = wrapper.querySelector('.header')
+    const closeBtn = wrapper.querySelector('#close')
+    let likesCount = 0
+    let followersCount = 0
+
+    closeBtn.addEventListener('click', e => {
+        dialog.close()
+        dialog.remove()
+    })
+
+    header.addEventListener('click', e => {
+        const _target = e.target
+
+        if (e.target.id == 'like') {
+            const h1 = likesContainer.querySelector('h1')
+
+            if (likesCount == 0) {
+                h1.textContent = ++likesCount
+                _target.textContent = 'Liked'
+                _target.classList.add('clicked')
+            } else {
+                h1.textContent = --likesCount
+                _target.textContent = 'Like'
+                _target.classList.remove('clicked')
+            }
+        }
+
+        if (e.target.id == 'follow') {
+            const h1 = followersContainer.querySelector('h1')
+            
+            if (followersCount == 0) {
+                h1.textContent = ++followersCount
+                _target.textContent = 'Followed'
+                _target.classList.add('clicked')
+            } else {
+                h1.textContent = --followersCount
+                _target.textContent = 'Follow'
+                _target.classList.remove('clicked')
+            }
+        }
+    })
 
     addBtn.addEventListener('click', e => {
         const input = document.createElement('input')
@@ -30,25 +74,24 @@ const init = () => {
 
     document.onscroll = function(e) {
         if (document.documentElement.scrollTop != 0) {
-            const dialog = document.querySelector('dialog')
-            angleUp.style.display = 'block'
-            
             if (dialog) {
                 dialog.style.display = 'none'
                 dialog.close()
             }
+
+            angleUp.style.display = 'block'
         } else {
             angleUp.style.display = 'none'
         }
         
         if ((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight) {
-            const dialog = document.querySelector('dialog')
-            dialog.style.display = 'block'
-            
-            if (typeof dialog.showModal === 'function') {
-                dialog.showModal()
-            } else {
-                alert('Dialog API is not supported...')
+            if (wrapper.contains(dialog)) {
+                if (typeof dialog.showModal === 'function') {
+                    dialog.style.display = 'block'
+                    dialog.showModal()
+                } else {
+                    alert('Dialog API is not supported...')
+                }
             }
         }
     }
